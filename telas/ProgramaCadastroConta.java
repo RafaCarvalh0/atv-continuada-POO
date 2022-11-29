791e5a39f8e9d09d8f1f1a61655fd1c0c69d52a6
Rafael Carvalho <99702583+RafaCarvalh0@users.noreply.github.com>
2022-11-29 16:02:34 -0300

    Delete telas directory

--- telas/ProgramaCadastroConta.java
+++
@@ -1,9 +0,0 @@
-package br.com.cesarschool.poo.telas;
-
-public class ProgramaCadastroConta {
-
-	public static void main(String[] args) {
-		TelaConta tela = new TelaConta();
-		tela.executaTela();
-	}
-}
